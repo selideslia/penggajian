09a64e615bb03d6a9895445009f799e8fb938d07
Seli Deslia <selideslia951@gmail.com>
2022-06-21 14:59:51 +0700

    commit pertama

---
+++ src/test/java/com/example/Penggajian/PenggajianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Penggajian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenggajianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
